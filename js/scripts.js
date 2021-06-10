6bca26df40aa475e280e2277e06e62ca84305781
“Christine <csalonen89@gmail.com>
2021-06-10 11:32:54 -0700

    add functions for clicks and toggle to display or hide paragraph text

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".card-title1").click(function() {
+    $("#jav-show").toggle();
+  });
+  $(".card-title2").click(function() {
+    $("#op-show").toggle();
+  });
+  $(".card-title3").click(function() {
+    $("#var-show").toggle();
+  });
+  $(".card-title4").click(function() {
+    $("#fun-show").toggle();
+  });
+  $(".card-title5").click(function() {
+    $("#method-show").toggle();
+  });
+  $(".card-title6").click(function() {
+    $("#arg-show").toggle();
+  });
+});
